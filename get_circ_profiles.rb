ed87f174f57e07167250aaff4e9d849e5955a3d2
Jon Gorman <jtgorman@illinois.edu>
2020-01-08 14:14:12 -0800

    Have poc dumping out the scopes for each entry in the people's yaml file

--- get_circ_profiles.rb
+++ get_circ_profiles.rb
@@ -10,6 +10,13 @@ def _setup_voyager_connection()
   OCI8.new( config['sid'] )
 end
 
+#TODO: refactor?
+def circ_profile_name_to_alma_scope(circ_profile_name)
+  circ_profile_name.upcase.gsub(/[[:space:]]/,'')
+end
+
+
+
 voyager = _setup_voyager_connection()
 
 
@@ -22,18 +29,42 @@ people = YAML::load_file('_data/staff_added_netids.yml')
 
 puts people
 
-people.each do | person |
-  person[:netids].each do | netid |
+scope_mapping_exceptions = YAML::load_file('circ_profile_name_to_alma_library.map') 
 
+
+puts scope_mapping_exceptions
+
+people.each do | person |
+  
+  scopes = Array.new
+  
+  person[:netids].each do | netid |
+    
     cursor.bind_param(:id, netid)
     cursor.exec
+    
+    cursor.fetch do | row |
+      circ_profile_name = row[0]
 
-    cursor.fetch do |row|
-      printf "%20s %20s\n",netid,row[0]
+      # looking like will need to refactor
+      # since some patterns are adding info after Level
+      if matches = circ_profile_name.match( /^[[:space:]]*(?<cpn_front>.*)[[:space:]]Level/ )
+        
+        
+        circ_profile_name_part = matches[:cpn_front]
+        
+        
+        if scope_mapping_exceptions.has_key?(circ_profile_name_part.downcase)
+          scopes = scopes + scope_mapping_exceptions[circ_profile_name_part.downcase]["codes"].map(&:to_s)
+        else
+          scopes.push( circ_profile_name_to_alma_scope( circ_profile_name_part ) )
+        end
+      end
     end
+    puts person[:uin] + "," + scopes.join(";")
   end
-end
 
+end
 
 
                            
