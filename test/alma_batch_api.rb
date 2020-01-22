015e111838bc5d78ca890131c67b05df29a67d95
Jon Gorman <jtgorman@illinois.edu>
2020-01-22 12:07:30 -0600

    Realized the control test case was testing each call took less than a second
    Changed so tested that 30 calls took less than a second, not using api
    but using http requests and mock test
    
    Added test that 30 requests via api took more than a second....will
    probably add to it to be more sure... (say do 100 requests in the first
    one

--- test/alma_batch_api.rb
+++ test/alma_batch_api.rb
@@ -4,15 +4,18 @@ require __dir__ + '/../lib/Alma/Batch/Api.rb'
 require 'uri'
 
 class TestAlmaBatchAPI < Test::Unit::TestCase
-  
+
+
+  # TODO: refactor out common code in to a test method so
+  # we just setup webmock and the expected timing results (maybe return times as array of int)
 
   def test_control
 
-    puts "control test started"
+#    puts "control test started"
     
     # this is complete guess work,
     # but will test 30 times and if 29 get done in less
-    # than a second, the reest of the tests are reasonable
+    # than a second, the rest of the tests are reasonable
     
     api = Alma::Batch::Api.new( __dir__ + '/test_alma.yml' )
     
@@ -25,26 +28,29 @@ class TestAlmaBatchAPI < Test::Unit::TestCase
     
     less_than_a_second_count = 0
 
+    start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC )
     (1..30).each do
-      start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC )
 
       uri = URI( 'https://foo/user/1' )
       request = Net::HTTP::Get.new( uri )
-      api.call( uri, request  )
+
+      # we DON'T want to call the api for this test...
       
-      if Process.clock_gettime(Process::CLOCK_MONOTONIC ) - start_time < 1 then
-        less_than_a_second_count += 1
-      end
+      
+      response = Net::HTTP.start(uri.hostname,
+                                 uri.port,
+                                 :use_ssl => true) { | http |
+        http.request( request )
+      }
     end
-    
-    assert_compare(less_than_a_second_count, '>', 29 ) 
-    
 
-    puts "control test ended"
+    assert_compare(Process.clock_gettime(Process::CLOCK_MONOTONIC ) - start_time, '<', 1 ) 
+    
+#    puts "control test ended"
   end
-
+  
   def test_pauses_after_per_second_warning
-
+    
     api = Alma::Batch::Api.new
 
     # if we time out every five, we'll need to make sure we have 31, because WebMock repeats the last request
@@ -60,7 +66,7 @@ class TestAlmaBatchAPI < Test::Unit::TestCase
        mock_response  
     }  
 
-    puts mock_responses
+#    puts mock_responses
     
     # set up webmock
     stub_request(:any, /.*/).to_return( mock_responses )
@@ -69,7 +75,7 @@ class TestAlmaBatchAPI < Test::Unit::TestCase
     more_than_a_second_count = 0
 
     (1..30).each do | position |
-      puts "At request #{position}" 
+#      puts "At request #{position}" 
       start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC )
 
       uri = URI(  'https://foo/user/1' )
@@ -78,13 +84,56 @@ class TestAlmaBatchAPI < Test::Unit::TestCase
       api.call( uri, request  )
       
       if position % 5 == 0 and (Process.clock_gettime(Process::CLOCK_MONOTONIC ) - start_time ) > 1 then
-        puts "got time delay"
+#        puts "got time delay"
         more_than_a_second_count += 1
       end
     end
     
     assert_compare(more_than_a_second_count, '>=' , 6 ) 
     
-    puts "per second message test ended"
+#    puts "per second message test ended"
   end
+
+
+
+  # we're testing the built-in threshold of 20 seconds per minute here...
+  # we should avoid hitting the threshold if we can without warnings from Alma..
+  def test_pauses_after_too_many_request_per_second
+
+    api = Alma::Batch::Api.new
+
+    # if we time out every five, we'll need to make sure we have 31, because WebMock repeats the last request
+    # feels like better way to do this...
+    mock_responses = (1..30).map { | position |
+      {status: 200,
+       body: "<user><primary_id>#{position.to_s}</primary_id></user>" } 
+    }  
+    
+    # set up webmock
+    stub_request(:any, /.*/).to_return( mock_responses )
+    
+    
+    
+    start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC )
+    
+    (1..30).each do | position |
+#      puts "At request #{position} at " + Time.now.to_s 
+      
+      
+      uri = URI(  'https://foo/user/1' )
+      request = Net::HTTP::Get.new( uri )
+      
+      api.call( uri, request  )
+      
+    end
+
+    time_elapsed = Process.clock_gettime( Process::CLOCK_MONOTONIC )  - start_time
+    assert_compare(time_elapsed, '>=' , 1 )
+    
+    
+#    puts "too many per second  test ended"
+  end
+
+
+  
 end
