caeb0e7986a6815c6ca3262eb7a8cf1ca430c3d3
Jon Gorman <jtgorman@illinois.edu>
2020-01-22 09:48:50 -0600

    Added api calls to test_control, now for the more advanced tests

--- test/alma_batch_api.rb
+++ test/alma_batch_api.rb
@@ -13,16 +13,20 @@ class TestAlmaBatchAPI < Test::Unit::TestCase
     
     api = Alma::Batch::Api.new
     
+
+    mock_responses = (1..30).map { | position | { body: position.to_s }}
+
+    # set up webmock
+    stub_request(:any, /.*/).to_return( mock_responses )
     
-    # set up webmock 
-    (1..30).each do
-      stub_request(:any, /.*/)
-    end
     
     less_than_a_second_count = 0
 
     (1..30).each do
       start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC )
+
+      request = Net::HTTP::Get.new( 'http://madeupdomains.com/forus' )
+      api.call( request )
       
       if Process.clock_gettime(Process::CLOCK_MONOTONIC ) - start_time < 1 then
         less_than_a_second_count += 1
@@ -33,7 +37,47 @@ class TestAlmaBatchAPI < Test::Unit::TestCase
     
     
   end
+
+
+
+#  def test_pauses_after_per_second_warning
+    
+    # this is complete guess work,
+    # but will test 30 times and if 29 get done in less
+    # than a second, the reest of the tests are reasonable
+    
+#    api = Alma::Batch::Api.new
+    
+    
+    # set up webmock 
+#    (1..30).each do | position |#
+#
+#      if position % 3 == 0
+#        stub_request(:any, /.*/)
+#      else
+#        stub_request(:any, /.*/)
+#      end
+#    end
+    
+#    less_than_a_second_count = 0
+#
+#    (1..30).each do
+#      start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC )
+#      
+#      if Process.clock_gettime(Process::CLOCK_MONOTONIC ) - start_time < 1 then
+#        less_than_a_second_count += 1
+#      end
+#    end
+#    
+#    assert_compare(less_than_a_second_count, '>', 29 ) 
+#    
+#    
+#  end
+
+
 end
+
+
   #  it 'pauses for one second if hits PER SECOND threshold' do
 #
 #    api = Alma::Batch::Api.new
